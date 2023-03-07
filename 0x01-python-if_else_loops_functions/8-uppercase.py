2c2015d3d3f664537a6cf9e228b203347fe8b8f4
Nyakallo <nyakallomasiu@gmail.com>
2023-03-07 03:59:35 -0800

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(s):
+    for c in s:
+        if ord('a') <= ord(c) <= ord('z'):
+            print(chr(ord(c) - 32), end='')
+        else:
+            print(c, end='')
+    print()

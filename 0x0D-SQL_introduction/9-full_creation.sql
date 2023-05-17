a1bdc5b7a86734df797e44bf854ec508e1240819
Nyakallo <nyakallomasiu@gmail.com>
2023-05-17 13:05:03 -0700

    script that lists all records of the table second_table of the database hbtn_0c_0

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,6 @@
 -- Creates a table second_table in the database hbtn_0c_0 and inserts values into the table
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (id INT,name VARCHAR(256),score INT);
-INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10),(2, 'Alex', 3),(3, 'Bob', 14),(4, 'George', 8);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex, 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);

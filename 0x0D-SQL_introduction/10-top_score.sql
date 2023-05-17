e14d3b83dd81daea5f17ad7a27a39d0edfd18160
Nyakallo <nyakallomasiu@gmail.com>
2023-05-17 13:34:38 -0700

    a script that lists all records of the table second_table of the database hbtn_0c_0

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- List all records of table "second_table"
+-- Select all records from the table and order them by score in descending order
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
